3d3c744b75c902b7820f4d69ca9ff271857ec82a
AsakawaSakuto <k024g1017@g.neec.ac.jp>
2026-06-16 19:18:50 +0900

    エフェクト名・パラメータ名を日本語化＆Jar.cpp追加
    LayerEffectPassSetup.cppのエフェクト名・パラメータ名を日本語表記に変更しました。また、Jar.cppをビルド対象に追加しました。

--- Project/Application/.Core/LayerEffectPassSetup.cpp
+++ Project/Application/.Core/LayerEffectPassSetup.cpp
@@ -42,38 +42,38 @@ namespace RenderPassSetup {
 
 		auto setupOutlineParameters = [](MadoEngine::Render::LayerEffectPass* pass) {
 			pass->SetParameterData(OutlineParams{});
-			pass->AddFloatParameterControl("Color R", offsetof(OutlineParams, outlineColor) + sizeof(float) * 0, 0.0f, 1.0f, 0.01f);
-			pass->AddFloatParameterControl("Color G", offsetof(OutlineParams, outlineColor) + sizeof(float) * 1, 0.0f, 1.0f, 0.01f);
-			pass->AddFloatParameterControl("Color B", offsetof(OutlineParams, outlineColor) + sizeof(float) * 2, 0.0f, 1.0f, 0.01f);
-			pass->AddFloatParameterControl("Thickness", offsetof(OutlineParams, outlineParams) + sizeof(float) * 0, 0.25f, 8.0f, 0.05f);
-			pass->AddFloatParameterControl("DepthSensitivity", offsetof(OutlineParams, outlineParams) + sizeof(float) * 1, 1.0f, 300.0f, 1.0f);
-			pass->AddFloatParameterControl("Threshold", offsetof(OutlineParams, outlineParams) + sizeof(float) * 2, 0.0001f, 0.1f, 0.001f);
-			pass->AddFloatParameterControl("Intensity", offsetof(OutlineParams, outlineParams) + sizeof(float) * 3, 0.0f, 3.0f, 0.01f);
+			pass->AddFloatParameterControl("輪郭色 R", offsetof(OutlineParams, outlineColor) + sizeof(float) * 0, 0.0f, 1.0f, 0.01f);
+			pass->AddFloatParameterControl("輪郭色 G", offsetof(OutlineParams, outlineColor) + sizeof(float) * 1, 0.0f, 1.0f, 0.01f);
+			pass->AddFloatParameterControl("輪郭色 B", offsetof(OutlineParams, outlineColor) + sizeof(float) * 2, 0.0f, 1.0f, 0.01f);
+			pass->AddFloatParameterControl("輪郭の太さ", offsetof(OutlineParams, outlineParams) + sizeof(float) * 0, 0.25f, 8.0f, 0.05f);
+			pass->AddFloatParameterControl("深度感度", offsetof(OutlineParams, outlineParams) + sizeof(float) * 1, 1.0f, 300.0f, 1.0f);
+			pass->AddFloatParameterControl("輪郭しきい値", offsetof(OutlineParams, outlineParams) + sizeof(float) * 2, 0.0001f, 0.1f, 0.001f);
+			pass->AddFloatParameterControl("輪郭の強さ", offsetof(OutlineParams, outlineParams) + sizeof(float) * 3, 0.0f, 3.0f, 0.01f);
 		};
 
 		MadoEngine::Render::LayerEffectPass::Desc defaultLayerPass{};
-		defaultLayerPass.name = "DefaultLayer";
+		defaultLayerPass.name = "デフォルトレイヤー";
 		defaultLayerPass.targetLayerMask = MadoEngine::Render::ToRenderLayerMask(MadoEngine::Render::RenderLayer::Default);
 		defaultLayerPass.effectShaderKey = "PostEffect/CopyImage.PS";
 		defaultLayerPass.enabled = false;
 		execution.AddLayerEffectPass(defaultLayerPass);
 
 		MadoEngine::Render::LayerEffectPass::Desc playerLayerPass{};
-		playerLayerPass.name = "PlayerLayer";
+		playerLayerPass.name = "プレイヤー白黒";
 		playerLayerPass.targetLayerMask = MadoEngine::Render::ToRenderLayerMask(MadoEngine::Render::RenderLayer::Player);
 		playerLayerPass.effectShaderKey = "PostEffect/GrayScale.PS";
 		playerLayerPass.enabled = true;
 		execution.AddLayerEffectPass(playerLayerPass);
 
 		MadoEngine::Render::LayerEffectPass::Desc playerSepiaPass{};
-		playerSepiaPass.name = "PlayerSepia";
+		playerSepiaPass.name = "プレイヤーセピア";
 		playerSepiaPass.targetLayerMask = MadoEngine::Render::ToRenderLayerMask(MadoEngine::Render::RenderLayer::Player);
 		playerSepiaPass.effectShaderKey = "PostEffect/Sepia.PS";
 		playerSepiaPass.enabled = true;
 		execution.AddLayerEffectPass(playerSepiaPass);
 
 		MadoEngine::Render::LayerEffectPass::Desc playerOutlinePass{};
-		playerOutlinePass.name = "PlayerOutline";
+		playerOutlinePass.name = "プレイヤー輪郭";
 		playerOutlinePass.targetLayerMask = MadoEngine::Render::ToRenderLayerMask(MadoEngine::Render::RenderLayer::Player);
 		playerOutlinePass.effectShaderKey = "PostEffect/Outline.PS";
 		playerOutlinePass.enabled = false;
@@ -81,45 +81,45 @@ namespace RenderPassSetup {
 		setupOutlineParameters(execution.AddLayerEffectPass(playerOutlinePass));
 
 		MadoEngine::Render::LayerEffectPass::Desc bloomPass{};
-		bloomPass.name = "Bloom";
+		bloomPass.name = "ブルーム";
 		bloomPass.targetLayerMask = MadoEngine::Render::kAllRenderLayers;
 		bloomPass.effectShaderKey = "PostEffect/Bloom.PS";
 		bloomPass.enabled = false;
 
 		MadoEngine::Render::LayerEffectPass* registeredBloomPass = execution.AddScreenEffectPass(bloomPass);
 		registeredBloomPass->SetParameterData(BloomParams{});
-		registeredBloomPass->AddFloatParameterControl("Intensity", offsetof(BloomParams, intensity), 0.0f, 5.0f, 0.01f);
-		registeredBloomPass->AddFloatParameterControl("Threshold", offsetof(BloomParams, threshold), 0.0f, 1.0f, 0.01f);
-		registeredBloomPass->AddFloatParameterControl("Radius", offsetof(BloomParams, radius), 0.0f, 32.0f, 0.1f);
-		registeredBloomPass->AddFloatParameterControl("SoftKnee", offsetof(BloomParams, softKnee), 0.0f, 1.0f, 0.01f);
+		registeredBloomPass->AddFloatParameterControl("発光の強さ", offsetof(BloomParams, intensity), 0.0f, 5.0f, 0.01f);
+		registeredBloomPass->AddFloatParameterControl("発光しきい値", offsetof(BloomParams, threshold), 0.0f, 1.0f, 0.01f);
+		registeredBloomPass->AddFloatParameterControl("ぼかし半径", offsetof(BloomParams, radius), 0.0f, 32.0f, 0.1f);
+		registeredBloomPass->AddFloatParameterControl("境界の柔らかさ", offsetof(BloomParams, softKnee), 0.0f, 1.0f, 0.01f);
 
 		MadoEngine::Render::LayerEffectPass::Desc vignettePass{};
-		vignettePass.name = "Vignette";
+		vignettePass.name = "周辺減光";
 		vignettePass.targetLayerMask = MadoEngine::Render::kAllRenderLayers;
 		vignettePass.effectShaderKey = "PostEffect/Vignette.PS";
 		vignettePass.enabled = false;
 
 		MadoEngine::Render::LayerEffectPass* registeredVignettePass = execution.AddScreenEffectPass(vignettePass);
 		registeredVignettePass->SetParameterData(VignetteParams{});
-		registeredVignettePass->AddFloatParameterControl("Strength", offsetof(VignetteParams, strength), 0.0f, 100.0f, 0.01f);
-		registeredVignettePass->AddFloatParameterControl("Radius", offsetof(VignetteParams, radius), 0.0f, 100.0f, 0.01f);
-		registeredVignettePass->AddFloatParameterControl("Smoothness", offsetof(VignetteParams, smoothness), 1.0f, 100.0f, 0.01f);
+		registeredVignettePass->AddFloatParameterControl("減光の強さ", offsetof(VignetteParams, strength), 0.0f, 100.0f, 0.01f);
+		registeredVignettePass->AddFloatParameterControl("減光半径", offsetof(VignetteParams, radius), 0.0f, 100.0f, 0.01f);
+		registeredVignettePass->AddFloatParameterControl("滑らかさ", offsetof(VignetteParams, smoothness), 1.0f, 100.0f, 0.01f);
 
 		MadoEngine::Render::LayerEffectPass::Desc pixelArtPass{};
-		pixelArtPass.name = "PixelArt";
+		pixelArtPass.name = "ドット絵風";
 		pixelArtPass.targetLayerMask = MadoEngine::Render::kAllRenderLayers;
 		pixelArtPass.effectShaderKey = "PostEffect/PixelArt.PS";
 		pixelArtPass.enabled = false;
 
 		MadoEngine::Render::LayerEffectPass* registeredPixelArtPass = execution.AddScreenEffectPass(pixelArtPass);
 		registeredPixelArtPass->SetParameterData(PixelArtParams{});
-		registeredPixelArtPass->AddFloatParameterControl("PixelSize", offsetof(PixelArtParams, pixelSize), 1.0f, 128.0f, 1.0f);
-		registeredPixelArtPass->AddFloatParameterControl("ColorSteps", offsetof(PixelArtParams, colorSteps), 2.0f, 128.0f, 1.0f);
-		registeredPixelArtPass->AddFloatParameterControl("Contrast", offsetof(PixelArtParams, contrast), 0.0f, 3.0f, 0.01f);
-		registeredPixelArtPass->AddFloatParameterControl("Intensity", offsetof(PixelArtParams, intensity), 0.0f, 1.0f, 0.01f);
+		registeredPixelArtPass->AddFloatParameterControl("ピクセルサイズ", offsetof(PixelArtParams, pixelSize), 1.0f, 128.0f, 1.0f);
+		registeredPixelArtPass->AddFloatParameterControl("色数", offsetof(PixelArtParams, colorSteps), 2.0f, 128.0f, 1.0f);
+		registeredPixelArtPass->AddFloatParameterControl("コントラスト", offsetof(PixelArtParams, contrast), 0.0f, 3.0f, 0.01f);
+		registeredPixelArtPass->AddFloatParameterControl("効果の強さ", offsetof(PixelArtParams, intensity), 0.0f, 1.0f, 0.01f);
 
 		MadoEngine::Render::LayerEffectPass::Desc screenOutlinePass{};
-		screenOutlinePass.name = "ScreenOutline";
+		screenOutlinePass.name = "画面全体輪郭";
 		screenOutlinePass.targetLayerMask = MadoEngine::Render::kAllRenderLayers;
 		screenOutlinePass.effectShaderKey = "PostEffect/Outline.PS";
 		screenOutlinePass.enabled = false;
