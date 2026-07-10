0032d73a7a9bf6c782cb5ee19affe14980415461
浅川咲人 (AsakawaSakuto) <k026g6002@g.neec.ac.jp>
2026-07-10 17:04:17 +0900

    Rarity列挙型にコメントを追加。

--- Project/Application/GameObject/Rarity.h
+++ Project/Application/GameObject/Rarity.h
@@ -2,12 +2,13 @@
 
 // @brief アイテムのレアリティや武器のアップグレードレアリティを表す列挙型
 enum class Rarity {
-	Uncommon,  // 緑
-	Rare,      // 青
-	Epic,      // 紫
-	Legendary, // 黄
 
-	Unique,    // 赤
+	Uncommon = 0,  // 緑
+	Rare,          // 青
+	Epic,          // 紫
+	Legendary,     // 黄 // 強化の選択肢に出るのはここまで
+			       
+	Unique,        // 赤	 // キャラ専用アイテム用
 
 	Count
 };
